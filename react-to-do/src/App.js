d2b65334c3e9f9f2f9890090b222d8eccdea8da0
An Mo <ann@amurraypr.com>
2018-01-02 20:53:24 -0800

    Checkpoint 7 react

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) { //construction function
+    super(props);
+    this.state = { //assign component's initial state
+       todos: [ //assign (state) object with todos property
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ] // each object is a to do with properties description and completed
+     }; // basically,  the to-dos  are stored in this.state.todos as an array of objects.
+  }
+
+
   render() {
     return (
       <div className="App">
       <ul>
-         <ToDo />
-         <ToDo />
+      { this.state.todos.map( (todo, index) =>
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+      )}
        </ul>
       </div>
     );
